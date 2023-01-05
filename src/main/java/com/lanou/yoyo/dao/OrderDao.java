6e23cf4ab06560773a74f53a7caec4f619f0c8fc
fygod <121625899+stellariumImpl@users.noreply.github.com>
2023-01-05 16:51:34 +0800

    Update OrderDao.java

--- src/main/java/com/lanou/yoyo/dao/OrderDao.java
+++ src/main/java/com/lanou/yoyo/dao/OrderDao.java
@@ -10,5 +10,18 @@ public interface OrderDao {
 	 * @return
 	 */
 	int insertOrder(Order order);
-
+	
+	/**
+	 * 根据订单id查询订单
+	 * @param orderId 订单id
+	 * @return 返回订单
+	 */
+	Order selectOrderById(int orderId);
+	
+	/**
+	 * 更新订单信息
+	 * @param order 要更新的订单
+	 * @return 如果更新成功 返回true
+	 */
+	int updateOrder(Order order);
 }
