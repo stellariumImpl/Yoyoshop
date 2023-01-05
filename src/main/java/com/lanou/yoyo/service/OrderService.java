e67b54f29dcc9226dfb2347d1d2ecf43f3c23125
fygod <121625899+stellariumImpl@users.noreply.github.com>
2023-01-05 16:49:26 +0800

    Update OrderService.java

--- src/main/java/com/lanou/yoyo/service/OrderService.java
+++ src/main/java/com/lanou/yoyo/service/OrderService.java
@@ -10,5 +10,19 @@ public interface OrderService {
 	 * @return
 	 */
 	int addOrder(Order order);
+	
+	/**
+	 * 根据订单id 获取订单信息
+	 * @param orderId
+	 * @return
+	 */
+	Order getOrderById(int orderId);
+	
+	/**
+	 * 更新订单信息
+	 * @param order 要更新的订单
+	 * @return 如果更新成功 返回true
+	 */
+	boolean updateOrder(Order order);
 
 }
