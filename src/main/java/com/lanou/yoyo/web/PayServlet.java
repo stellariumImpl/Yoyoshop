a1827525bb35f6888f847c9f5c4ea15593a8d101
fygod <121625899+stellariumImpl@users.noreply.github.com>
2023-01-06 09:34:14 +0800

    Update PayServlet.java
    扣减库存

--- src/main/java/com/lanou/yoyo/web/PayServlet.java
+++ src/main/java/com/lanou/yoyo/web/PayServlet.java
@@ -10,11 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.lanou.yoyo.bean.Item;
 import com.lanou.yoyo.bean.Order;
 import com.lanou.yoyo.bean.Type;
 import com.lanou.yoyo.bean.User;
+import com.lanou.yoyo.service.GoodsService;
+import com.lanou.yoyo.service.ItemService;
 import com.lanou.yoyo.service.OrderService;
 import com.lanou.yoyo.service.TypeService;
+import com.lanou.yoyo.service.impl.GoodsServiceImpl;
+import com.lanou.yoyo.service.impl.ItemServiceImpl;
 import com.lanou.yoyo.service.impl.OrderServiceImpl;
 import com.lanou.yoyo.service.impl.TypeServiceImpl;
 
@@ -26,6 +31,10 @@ public class PayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private OrderService orderService = new OrderServiceImpl();
 	private TypeService typeService = new TypeServiceImpl();
+	private ItemService itemService = new ItemServiceImpl();
+	
+	//减库存
+	private GoodsService goodsService = new GoodsServiceImpl();
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -34,7 +43,7 @@ public class PayServlet extends HttpServlet {
 		HttpSession session = request.getSession();
 		User user = (User)session.getAttribute("user");
 		if(user==null) {
-			response.sendRedirect("/Yoyoshop/index/login");
+			response.sendRedirect("/fygod_shop/index/login");
 		}else {
 			request.setCharacterEncoding("UTF-8");
 			//获取请求参数id,paytype,name,phone,address
@@ -65,14 +74,17 @@ public class PayServlet extends HttpServlet {
 			boolean isSuccess = orderService.updateOrder(order);
 			
 			if(isSuccess) {//如果订单更新成功
+				//根据订单id获取购买项列表
+				List<Item> itemList = itemService.getItemListByOrderId(orderId);
+				//
+				goodsService.updateGoodsStockForItemList(itemList);
+				
+				
 				//设置成功的提示信息
 				request.setAttribute("msg", "支付成功！");
 			}else {//订单更新失败
 				request.setAttribute("msg", "支付失败了，请检查收货人以及收货地址");
 			}
-			
-			session.removeAttribute("order");
-			
 			List<Type> typeList=typeService.getTypeList();
 			request.setAttribute("typeList", typeList);
 			
