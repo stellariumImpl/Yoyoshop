81115bf472569e4463bf08a9d0343812e4ed1194
fygod <121625899+stellariumImpl@users.noreply.github.com>
2023-01-06 16:02:29 +0800

    Update PlaceOrderServlet.java
    work complete

--- src/main/java/com/lanou/yoyo/web/PlaceOrderServlet.java
+++ src/main/java/com/lanou/yoyo/web/PlaceOrderServlet.java
@@ -18,13 +18,13 @@ import com.lanou.yoyo.bean.Order;
 import com.lanou.yoyo.bean.Type;
 import com.lanou.yoyo.bean.User;
 import com.lanou.yoyo.service.GoodsService;
-import com.lanou.yoyo.service.ItemService;
 import com.lanou.yoyo.service.OrderService;
+import com.lanou.yoyo.service.ItemService;
 import com.lanou.yoyo.service.TypeService;
 import com.lanou.yoyo.service.impl.GoodsServiceImpl;
-import com.lanou.yoyo.service.impl.ItemServiceImpl;
 import com.lanou.yoyo.service.impl.OrderServiceImpl;
 import com.lanou.yoyo.service.impl.TypeServiceImpl;
+import com.lanou.yoyo.service.impl.ItemServiceImpl;
 
 /**
  * Servlet implementation class PlaceOrderServlet
@@ -32,35 +32,53 @@ import com.lanou.yoyo.service.impl.TypeServiceImpl;
 @WebServlet("/index/placeOrder")
 public class PlaceOrderServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private GoodsService goodsService = new GoodsServiceImpl();
+
 	private TypeService typeService = new TypeServiceImpl();
+
+	private GoodsService goodsService = new GoodsServiceImpl();
+
 	private OrderService orderService = new OrderServiceImpl();
+
 	private ItemService itemService = new ItemServiceImpl();
+
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		List<Type> typeList = typeService.getTypeList();
+		request.setAttribute("typeList", typeList);
+
 		HttpSession session = request.getSession();
-		User user = (User)session.getAttribute("user");
-		if(user == null) {
-			response.sendRedirect("YoyoShop/index/login");
-		}else {
-			Cart cart = (Cart)session.getAttribute("order");
+		User user = (User) session.getAttribute("user");
+
+		if (user == null) {
+			request.setAttribute("msg", "请登录后再提交订单！");
+			response.sendRedirect("/YoyoShop/index/login");
+		} else {
+			Cart cart = (Cart) session.getAttribute("order");
 			List<Item> itemList = cart.getItemList();
-			for(int i = 0; i < itemList.size(); i++) {
+			for (int i = 0; i < itemList.size(); i++) {
+				// 获取购买项
 				Item item = itemList.get(i);
 				int amount = item.getAmount();
 				int goodsId = item.getGoodId();
 				Goods goods = goodsService.getGoodsById(goodsId);
 				int stock = goods.getStock();
-				if(amount > goods.getStock()) {
-					request.setAttribute("msg", "下单失败,商品[" + goods.getName() + "]库存不足");
-				    List<Type> typeList = typeService.getTypeList();
-				    request.setAttribute("typeList", typeList);
-					request.getRequestDispatcher("index/cart.jsp").forward(request, response);
-				    return;
+				if (amount > stock) {
+					request.setAttribute("msg", "下单失败，商品 [" + goods.getName() + "] 库存不足，库存余量为 " + stock + " 件");
+
+					List<Type> typeList1 = typeService.getTypeList();
+					request.setAttribute("typeList", typeList1);
+					request.getRequestDispatcher("/index/cart.jsp").forward(request, response);
+					return;
+
 				}
 			}
+
+			// 库存充足
 			Order order = new Order();
 			order.setTotal(cart.getTotal());
 			order.setAmount(cart.getAmount());
@@ -68,20 +86,29 @@ public class PlaceOrderServlet extends HttpServlet {
 			order.setPaytype(0);
 			order.setSystime(new Date());
 			order.setUserId(cart.getUserId());
+
+			// 添加订单
 			int orderId = orderService.addOrder(order);
+
+			System.out.println("订单的id是: " + orderId);
+			// to be continued
+			// 把订单id设置给订单对象
 			order.setId(orderId);
-			for(int i = 0; i< itemList.size(); i++) {
+			// 把订单中的商品itemList加入到数据库的item表中
+			for (int i = 0; i < itemList.size(); i++) {
 				Item item = itemList.get(i);
-				item.setOrderId(orderId);
+				// 给购买项添加订单id
+				item.setOrderId(orderId);// 此时的购买项已经有了订单id
 			}
+			// 把订单中的商品itemList
 			itemService.addItemList(itemList);
+
+			// 清空购物车
 			session.removeAttribute("order");
+
 			request.setAttribute("order", order);
-			List<Type> typeList = typeService.getTypeList();
-			request.setAttribute("typeList", typeList);
 			request.getRequestDispatcher("/index/pay.jsp").forward(request, response);
 		}
-		
 	}
 
 }
