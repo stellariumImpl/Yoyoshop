94fbfc03d6c548aa04fb24c500f95500e48668df
fygod <121625899+stellariumImpl@users.noreply.github.com>
2023-01-06 09:39:11 +0800

    Update ItemDao.java
    扣减库存

--- src/main/java/com/lanou/yoyo/dao/ItemDao.java
+++ src/main/java/com/lanou/yoyo/dao/ItemDao.java
@@ -1,5 +1,7 @@
 package com.lanou.yoyo.dao;
 
+import java.util.List;
+
 import com.lanou.yoyo.bean.Item;
 
 public interface ItemDao {
@@ -10,5 +12,12 @@ public interface ItemDao {
 	 * @return 返回影响的条数，添加失败返回0，成功1
 	 */
 	int insertItem(Item item);
+	
+	/**
+	 * 根据订单id获取购买项列表
+	 * @param orderId 订单id
+	 * @return 返回购买项列表
+	 */
+	List<Item> selectItemListByOrderId(int orderId);
 
 }
