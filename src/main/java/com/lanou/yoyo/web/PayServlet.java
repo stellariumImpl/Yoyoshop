0cee4510bc649fb0a55562cbffe307bf6e287691
fygod <121625899+stellariumImpl@users.noreply.github.com>
2023-01-06 16:01:40 +0800

    Update PayServlet.java
    accomplish

--- src/main/java/com/lanou/yoyo/web/PayServlet.java
+++ src/main/java/com/lanou/yoyo/web/PayServlet.java
@@ -31,43 +31,67 @@ public class PayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private OrderService orderService = new OrderServiceImpl();
 	private TypeService typeService = new TypeServiceImpl();
-	private GoodsService goodsService = new GoodsServiceImpl();
 	private ItemService itemService = new ItemServiceImpl();
- 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+
+	// 减库存
+	private GoodsService goodsService = new GoodsServiceImpl();
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		User user = (User)session.getAttribute("user");
-		if(user == null) {
+		User user = (User) session.getAttribute("user");
+		if (user == null) {
 			response.sendRedirect("/YoyoShop/index/login");
-		}else {
+		} else {
 			request.setCharacterEncoding("UTF-8");
+			// 获取请求参数id,paytype,name,phone,address
 			String orderIdStr = request.getParameter("id");
 			String paytypeStr = request.getParameter("paytype");
-			String name= request.getParameter("name");
+			String name = request.getParameter("name");
 			String phone = request.getParameter("phone");
 			String address = request.getParameter("address");
+
 			int orderId = Integer.parseInt(orderIdStr);
 			int paytype = Integer.parseInt(paytypeStr);
+
 			Order order = orderService.getOrderById(orderId);
+			/**
+			 * 实际情况还需要
+			 */
+			// 设置订单状态为2 已付款
 			order.setStatus(2);
+
+			// 设置支付方式
 			order.setPaytype(paytype);
+			// 个人信息
+			order.setName(name);
 			order.setPhone(phone);
 			order.setAddress(address);
+
+			// 更新订单信息
 			boolean isSuccess = orderService.updateOrder(order);
-			if(isSuccess) {
+
+			if (isSuccess) {// 如果订单更新成功
+				// 根据订单id获取购买项列表
 				List<Item> itemList = itemService.getItemListByOrderId(orderId);
+				//
 				goodsService.updateGoodsStockForItemList(itemList);
-				
-				request.setAttribute("msg","恭喜你支付成功");
-				
-			}else {
-				request.setAttribute("msg", "支付失败,收货人或者地址过长");
-				
+
+				// 设置成功的提示信息
+				request.setAttribute("msg", "支付成功！");
+			} else {// 订单更新失败
+				request.setAttribute("msg", "支付失败了，请检查收货人以及收货地址");
 			}
+
+			session.removeAttribute("order");
+
 			List<Type> typeList = typeService.getTypeList();
 			request.setAttribute("typeList", typeList);
+
 			request.getRequestDispatcher("/index/payok.jsp").forward(request, response);
 		}
 	}
