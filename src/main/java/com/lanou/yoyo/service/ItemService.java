2301d229545e8a563764be77b235d2f39100a437
fygod <121625899+stellariumImpl@users.noreply.github.com>
2023-01-06 09:38:29 +0800

    Update ItemService.java
    扣减库存

--- src/main/java/com/lanou/yoyo/service/ItemService.java
+++ src/main/java/com/lanou/yoyo/service/ItemService.java
@@ -4,11 +4,18 @@ import java.util.List;
 
 import com.lanou.yoyo.bean.Item;
 
-/**
- * 把购买项添加到item列表中
- * @author FYGOD
- *
- */
 public interface ItemService {
+	/**
+	 * 把购买项添加到item列表中
+	 * @author FYGOD
+	 *
+	 */
 	void addItemList(List<Item> itemList);
+	
+	/**
+	 * 根据订单id获取购买项列表
+	 * @param orderId 订单id
+	 * @return 返回购买项列表
+	 */
+	List<Item> getItemListByOrderId(int orderId);
 }
